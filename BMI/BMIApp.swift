2d702e14b7f1b7a0fddde5dc211d3ce1ded9e4b4
Swapnil Bhattacharya <swapnilbhattacharya01@gmail.com>
2023-10-26 22:49:18 +0100

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Swapnil Bhattacharya on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
